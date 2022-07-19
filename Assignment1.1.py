a4ecb8b9e6a8efc011179036afc03d5b85955d81
Viperrakesh <108540120+Viperrakesh@users.noreply.github.com>
2022-07-19 23:03:55 +0530

    Add files via upload

---
+++ Assignment1.1.py
@@ -0,0 +1,9 @@
+#Assignment1.1
+#Program_to_get_the_Fibonacci_series_between 0 to 50
+
+starting_num = 0   #Series_starts_from 0
+ending_num = 1
+
+while ending_num<50:  #Series_ends_at_50
+    print(ending_num)
+    starting_num, ending_num = ending_num, starting_num+ending_num
