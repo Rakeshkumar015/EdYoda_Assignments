f0fb1cfddb195ffb383190cc4f6134887c13b1fa
Rakesh Kumar Behera <108540120+Viperrakesh@users.noreply.github.com>
2022-08-02 22:58:22 +0530

    Add files via upload

---
+++ Assignment_3.2.py
@@ -0,0 +1,14 @@
+#Assignment_3.2
+#String_inside_the_Function
+#Program_to_reverse_a_string
+
+def rev_str():
+    str1 =""    
+    for i in str2:  #Using_For_loop_to_reverse
+        str1 = i + str1     #Assigning_value_of_i_into_empty_string
+    return str1
+
+str2 = input("Enter a String: ")    #User_have_to_input_a_string
+
+call = rev_str()
+print("Reverse String:",call)
